4151ad4c9634411f54d61ef224777642cda17cd1
홍성욱 <suh75321@naver.com>
2024-01-19 17:54:52 +0900

    update

--- src/main/kotlin/com/teamsparta/todo/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/todo/infra/security/SecurityConfig.kt
@@ -1,21 +1,41 @@
 package com.teamsparta.todo.infra.security
 
+import com.teamsparta.todo.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntrypoint: AuthenticationEntryPoint
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .httpBasic { it.disable() } // BasicAuthenticationFilter, DefaultLoginPageGeneratingFilter, DefaultLogoutPageGeneratingFilter 제외
-            .formLogin { it.disable() } // UsernamePassworedAuthenticationFilter, DefaultLoginPageGeneratingFilter, DefaultLogoutPageGeneratingFilter 제외
-            .csrf { it.disable() } // CsrfFilter 제외
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/login",
+                    "/signup",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
+                ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            // 예외처리
+            .exceptionHandling{
+                it.authenticationEntryPoint(authenticationEntrypoint)
+            }
             .build()
     }
 
