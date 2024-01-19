23306865863cb393ca55fb3104cf66ae2af923df
홍성욱 <suh75321@naver.com>
2024-01-19 20:36:25 +0900

    update2

--- src/main/kotlin/com/teamsparta/todo/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/todo/infra/security/SecurityConfig.kt
@@ -3,17 +3,21 @@ package com.teamsparta.todo.infra.security
 import com.teamsparta.todo.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
-    private val authenticationEntrypoint: AuthenticationEntryPoint
+    private val authenticationEntrypoint: AuthenticationEntryPoint,
+    private val accessDeniedHandler: AccessDeniedHandler
 ) {
 
     @Bean
@@ -27,14 +31,14 @@ class SecurityConfig(
                     "/login",
                     "/signup",
                     "/swagger-ui/**",
-                    "/v3/api-docs/**"
+                    "/v3/api-docs/**",
                 ).permitAll()
                     .anyRequest().authenticated()
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
-            // 예외처리
             .exceptionHandling{
                 it.authenticationEntryPoint(authenticationEntrypoint)
+                it.accessDeniedHandler(accessDeniedHandler) // 추가!!
             }
             .build()
     }
